37de6755c95441ef88caa6662d10df9029e893ba
Colin <crrood@gmail.com>
2024-12-21 11:24:37 -0800

    Add -b flag to run.sh to make build and volume creation optional

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 docker compose --env-file webapp-client/shared/.env down
-docker volume create mongodb
-docker volume create caddy
-docker compose --env-file webapp-client/shared/.env build
+while getopts "b" opt; do
+  case $opt in
+    b)
+      docker volume create mongodb
+      docker volume create caddy
+      docker compose --env-file webapp-client/shared/.env build
+      ;;
+  esac
+done
 docker compose --env-file webapp-client/shared/.env up
