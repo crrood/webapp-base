dfa5dfbec800d8f5bc9eee3de206a8dae805ca8b
Colin <crrood@gmail.com>
2025-01-30 17:15:29 -0800

    Update run.sh with -v flag to reset named volumes

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
-docker compose --env-file webapp-client/shared/.env down
-while getopts "b" opt; do
+docker compose --env-file webapp-client/shared/.env down --volumes
+docker volume create mongodb
+docker volume create caddy
+while getopts ":b:v" opt; do
   case $opt in
-    b)
+    v)
+      docker volume rm mongodb caddy
       docker volume create mongodb
       docker volume create caddy
+      ;;
+    b)
       docker compose --env-file webapp-client/shared/.env build
       ;;
   esac
 done
-docker compose --env-file webapp-client/shared/.env up
+docker compose --env-file webapp-client/shared/.env up --abort-on-container-exit
